470a50f104d358c43cebf5513d6b065730c641fd
Amirr95 <arrezvaniyan@gmail.com>
2023-11-20 13:23:18 +0330

    query weather data

--- weather_api/database.py
+++ weather_api/database.py
@@ -3,7 +3,7 @@ import os
 import datetime as dt
 import json
 
-from utils.logger import logger
+from .utils.logger import logger
 
 class Database:
     def __init__(self) -> None:
@@ -14,7 +14,7 @@ class Database:
         self.post = self.db["postHarvestData"]
         self.pre = self.db["preHarvestData"]
         
-    def load_weather(self, date: dt.datetime):
+    def load_data(self, date: dt.datetime):
         file_names = [
             f"data/Iran{date.strftime('%Y%m%d')}_weather.geojson",
             f"data/Iran{date.strftime('%Y%m%d')}_AdviseSP.geojson",
@@ -31,7 +31,7 @@ class Database:
                 
                 for feature in features:
                     properties = feature["properties"]
-                    location = {"geometry": feature["geometry"]}
+                    location = {"location": feature["geometry"]}
                     time_properties = {key: properties[key] for key in properties.keys() if "Time=" in key}
                     timestamp = {"timestamp": date.strftime("%Y%m%d")}
                     entry = {**location, **timestamp, **time_properties}
@@ -42,11 +42,58 @@ class Database:
             except FileNotFoundError:
                 logger.error(f"File not found: {file}")
         
+    def query_weather(self, 
+                      longitude: float, 
+                      latitude: float, 
+                      max_distance: float = 10000, 
+                      date: dt.datetime = dt.datetime.now(), 
+                      **kwargs) -> dict:
+        res = self.weather.find_one(
+            {
+                "location":
+                    {
+                        "$nearSphere":
+                            {
+                                "$geometry": {"type": "Point", "coordinates": [longitude, latitude]},
+                                "$maxDistance": max_distance
+                            }
+                    },
+                "timestamp": date.strftime("%Y%m%d")
+            }
+        )
+        if res:
+            days = [key.split("=")[1] for key in list(res.keys())[:7] if "Time" in key]
+            f_days = [dt.datetime.strptime(day, "%Y%m%d").strftime("%Y-%m-%d") for day in days]
+            out = {
+                "latitude": res.get("location", {}).get("coordinates")[1],
+                "longitude": res.get("location", {}).get("coordinates")[0],
+                "generation_time": date.strftime('%Y-%m-%d %H:%M'),
+                "units": {
+                    "max_temperature": "°C",
+                    "min_temperature": "°C",
+                    "relative_humidity": "%",
+                    "precipitation_probability": "%",
+                    "wind_speed": "km/h"
+                },
+                "daily_values": {
+                    "dates": f_days,
+                    "max_temperature": [res.get(key) for key in list(res.keys()) if key.startswith("tmax")],
+                    "min_temperature": [res.get(key) for key in list(res.keys()) if key.startswith("tmin")],
+                    "relative_humidity": [res.get(key) for key in list(res.keys()) if key.startswith("rh")],
+                    "precipitation_probability": [res.get(key) for key in list(res.keys()) if key.startswith("rain")],
+                    "wind_speed": [res.get(key) for key in list(res.keys()) if key.startswith("spd")]
+                }
+            }
+            return out
+        else:
+            return {"msg": "weather data not found"}
+
 
 if __name__=="__main__":
     db = Database()
     date = dt.date(2023, 10, 17)
-    db.load_weather(date)
+    date = dt.datetime.now()
+    db.load_data(date)
         
         
    
